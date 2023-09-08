4ea654b785017ddc2dfb394407d202e837da887b
Ashaduzzaman10 <infoaj549814@gmail.com>
2023-09-08 17:22:10 +0600

    phone

--- js/phone.js
+++ js/phone.js
@@ -1,8 +1,24 @@
-document.getElementById("btn-phone-plus").addEventListener("click", function () {
+function updatePhoneNumber(isIncrease) {
   const phoneNumberField = document.getElementById("phone-number-field");
   const phoneNumberFieldString = phoneNumberField.value;
   const previousPhoneNumber = parseInt(phoneNumberFieldString);
-  const newPhoneNumber = previousPhoneNumber + 1;
+  let newPhoneNumber;
+  if (isIncrease === true) {
+     newPhoneNumber = previousPhoneNumber + 1;
+  } else {
+    newPhoneNumber = previousPhoneNumber - 1;
+  }
   phoneNumberField.value = newPhoneNumber;
   console.log(phoneNumberField);
+  return newPhoneNumber;
+}
+document
+  .getElementById("btn-phone-plus")
+  .addEventListener("click", function () {
+    updatePhoneNumber(true);
+  });
+
+
+document.getElementById("btn-phone-minus").addEventListener("click", function () {
+  updatePhoneNumber(false);
 })
