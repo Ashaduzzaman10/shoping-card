f887bd4a3ce1d8d5818ac1ca4fa8533bf7007d0b
Ashaduzzaman10 <infoaj549814@gmail.com>
2023-09-08 16:30:06 +0600

    degsin

---
+++ js/case.js
@@ -0,0 +1,8 @@
+document.getElementById("btn-case-plus").addEventListener("click", function () {
+  const caseNumberField = document.getElementById("case-number-field");
+  const caseNumberFieldString = caseNumberField.value;
+  const caseNumber = parseInt(caseNumberFieldString);
+  const newCaseNumber = caseNumber + 1;
+  caseNumberField.value = newCaseNumber;
+  console.log( newCaseNumber);
+});
