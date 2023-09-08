f62c6c65ca61b49be2bab35cec3aa88eeb8f8373
Ashaduzzaman10 <infoaj549814@gmail.com>
2023-09-08 17:16:12 +0600

    phone

---
+++ js/phone.js
@@ -0,0 +1,8 @@
+document.getElementById("btn-phone-plus").addEventListener("click", function () {
+  const phoneNumberField = document.getElementById("phone-number-field");
+  const phoneNumberFieldString = phoneNumberField.value;
+  const previousPhoneNumber = parseInt(phoneNumberFieldString);
+  const newPhoneNumber = previousPhoneNumber + 1;
+  phoneNumberField.value = newPhoneNumber;
+  console.log(phoneNumberField);
+})
