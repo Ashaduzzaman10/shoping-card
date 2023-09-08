b8acd761749841152d466908b6ab23f3c1671d5d
Ashaduzzaman10 <infoaj549814@gmail.com>
2023-09-08 16:38:36 +0600

    case

--- js/case.js
+++ js/case.js
@@ -4,5 +4,15 @@ document.getElementById("btn-case-plus").addEventListener("click", function () {
   const caseNumber = parseInt(caseNumberFieldString);
   const newCaseNumber = caseNumber + 1;
   caseNumberField.value = newCaseNumber;
-  console.log( newCaseNumber);
+  console.log(newCaseNumber);
 });
+document
+  .getElementById("btn-case-minus")
+  .addEventListener("click", function () {
+    const caseNumberField = document.getElementById("case-number-field");
+    const caseNumberFieldString = caseNumberField.value;
+    const caseNumber = parseInt(caseNumberFieldString);
+    const newCaseNumber = caseNumber - 1;
+    caseNumberField.value = newCaseNumber;
+    console.log(newCaseNumber);
+  });
